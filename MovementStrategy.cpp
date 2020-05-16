05bb371d11a981721f5eeec46e38c182fd768f2c
N <naahmu@gmail.com>
2020-05-16 10:35:41 +0200

    Edited makefile

---
+++ MovementStrategy.cpp
@@ -0,0 +1,5 @@
+//
+// Created by N on 16/05/2020.
+//
+
+#include "MovementStrategy.h"
---
+++ MovementStrategy.h
@@ -0,0 +1,16 @@
+//
+// Created by N on 16/05/2020.
+//
+
+#ifndef VIRAL_SIMULATION_MOVEMENTSTRATEGY_H
+#define VIRAL_SIMULATION_MOVEMENTSTRATEGY_H
+
+
+class MovementStrategy {
+public:
+    virtual ~MovementStrategy(){}
+    virtual std::string
+};
+
+
+#endif //VIRAL_SIMULATION_MOVEMENTSTRATEGY_H
